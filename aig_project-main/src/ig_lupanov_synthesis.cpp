7ab8efe335a0ce7106ca9f65846e111d8a2975bd
mishbash <138617704+mishbash@users.noreply.github.com>
2024-04-21 09:49:26 +0300

    Update ig_lupanov_synthesis.cpp

--- aig_project-main/src/ig_lupanov_synthesis.cpp
+++ aig_project-main/src/ig_lupanov_synthesis.cpp
@@ -69,17 +69,113 @@ void IGLupanovSynthesis::build_conjunction_set_naive() {
     border_of_cus = stop_period;
 }
 
+int IGLupanovSynthesis::build_pdnf(std::vector<IGLupanovSubFunction>& disjuncts) {
+    std::vector<int> nodes_to_add;
+
+    for (auto disjunct : disjuncts) {
+        if (!disjunct.value) continue;
+
+        bool k_bitmask = 0;
+
+        for (auto i : std::views::iota(0u, k_first)) {
+            k_bitmask |= disjunct.truth_table[i] ? (1 << i) : 0; // !!!!
+        }
+
+        nodes_to_add.push_back(border_before_cus + k_bitmask);
+    }
+
+    if (nodes_to_add.size() == 0) {
+        return -1;
+    }
+
+    if (nodes_to_add.size() == 1) {
+        return nodes_to_add[0];
+    }    
+
+    for (int i = 1; i < nodes_to_add.size(); ++i) {
+        if (i == 1) {
+            auto num = scheme.addNode(IGNode({nodes_to_add[0], nodes_to_add[1]}, {1, 1}, {0, 0, 0, 1}));
+            scheme.addNode(IGNode({num, num}, {1, 1}, {0, 0, 0, 1}));
+        } else {
+            auto num = scheme.addNode(IGNode({scheme.getLastNodeNum(), nodes_to_add[i]}, {1, 1}, {0, 0, 0, 1}));
+            scheme.addNode(IGNode({num, num}, {1, 1}, {0, 0, 0, 1}));
+        }
+    }
+
+    return scheme.getLastNodeNum();
+}
+
 void IGLupanovSynthesis::build() {
     scheme.setVariableCount(variables_count);
     build_conjunction_set_naive();
 
-    // std::vector<IGLupanovSubFunctionsList> sub_func_list;
 
-    // for (auto bitmask : std::views::iota(0u, 1u << )) { 
-    // }
+    // setup column per one secondary bitset
+
+    std::vector<int> disjuncts_nodes;
+    for (auto nk_bitmask : std::views::iota(0u, 1u << nk_last)) { // (... V ... V ... V ...) & bitset
+        std::vector<IGLupanovSubFunction> disjuncts;
+        std::vector<bool> binary_set(variables_count, 0);
+        for (auto i : std::views::iota(0u, nk_last)) {
+            binary_set[i + k_first] = nk_bitmask & (1 << i);
+        }
+
+        for (auto k_bitmask: std::views::iota(0u, 1u << k_first)) {
+            std::vector<bool> disjunct_truth_table(k_first);
+            for (auto i : std::views::iota(0u, k_first)) {
+                binary_set[i]           = k_bitmask & (1 << i);
+                disjunct_truth_table[i] = k_bitmask & (1 << i);
+            }   
+            
+            auto res = function.getValue(binary_set);
+            disjuncts.push_back({disjunct_truth_table, res});
+        }
+
+        /* SOME OUTPUT FOR TEST*/
+        // std::cout << "For secondary mask: ";
+        // for (auto i : std::views::iota(0u, nk_last)) {
+        //     std::cout << binary_set[i + k_first];
+        // }
+        // std::cout << "\n";
 
 
-    info();
+        // for every colomn we calculate disjunct
+        // then combine with nk_last
+        
+        auto node_to_add = build_pdnf(disjuncts);
+
+
+        if (node_to_add != -1) {
+            auto last = scheme.addNode(IGNode({node_to_add, border_start_of_second_cus + nk_bitmask}, {0, 0}, {0,0,0,1}));
+            disjuncts_nodes.push_back(last);
+        }   
+    }
+
+
+    if (disjuncts_nodes.size() == 0) {
+        scheme.addNode(IGNode({1, 1}, {1, 0}, {0, 0, 0, 1})); // all 0 function
+        scheme.setOutput(scheme.getLastNodeNum());
+        return;
+    }
+
+    if (disjuncts_nodes.size() == 1) {
+        scheme.setOutput(scheme.getLastNodeNum());
+        return ;
+    }    
+
+    for (int i = 1; i < disjuncts_nodes.size(); ++i) {
+        if (i == 1) {
+            auto num = scheme.addNode(IGNode({disjuncts_nodes[0], disjuncts_nodes[1]}, {1, 1}, {0, 0, 0, 1}));
+            scheme.addNode(IGNode({num, num}, {1, 1}, {0, 0, 0, 1}));
+        } else {
+            auto num = scheme.addNode(IGNode({scheme.getLastNodeNum(), disjuncts_nodes[i]}, {1, 1}, {0, 0, 0, 1}));
+            scheme.addNode(IGNode({num, num}, {1, 1}, {0, 0, 0, 1}));
+        }
+    }
+
+    scheme.setOutput(scheme.getLastNodeNum());
+
+    // info();
 
 }
 
@@ -97,4 +193,9 @@ void IGLupanovSynthesis::scheme_print(std::ostream& ostream, bool additional_inf
 
 void IGLupanovSynthesis::MKSprint(std::ostream& ostream) {
     scheme.MKSprint(ostream);
+}
+
+void IGLupanovSynthesis::getSchemeFunction(std::ostream& ostream) {
+    auto res = scheme.getResult();
+    res.print(ostream);
 }
